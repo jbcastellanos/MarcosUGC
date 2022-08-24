58d31451b7679337edec6d6c416dfbd785cd28dd
Felipevipa <felipevipa@outlook.com>
2022-08-24 14:52:39 -0500

    serialized some test

---
+++ Backend/ApiImagenes/ApiImagenesApp/serializers.py
@@ -0,0 +1,15 @@
+from dataclasses import field
+from statistics import mode
+from django.contrib.auth.models import User, Group
+from rest_framework import serializers
+
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = User
+        fields = ['url', 'username', 'email', 'groups']
+
+
+class GroupSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Group
+        fields = ['url', 'name',]
