dd0364532b8a4257bc19e0d34878b1ccbb413acc
Felipevipa <felipevipa@outlook.com>
2022-08-24 20:33:47 -0500

    updated to upload and get images from admin, created models

--- Backend/ApiImagenes/ApiImagenes/urls.py
+++ Backend/ApiImagenes/ApiImagenes/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import include, path
+from django.conf import settings
+from django.conf.urls.static import static
 from rest_framework import routers
 from rest_framework_simplejwt import views as jwt_views
 from ApiImagenesApp import views
@@ -14,4 +16,4 @@ urlpatterns = [
     path('api/token/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', jwt_views.TokenRefreshView.as_view()),
     path('admin/', admin.site.urls),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- Backend/ApiImagenes/ApiImagenesApp/models.py
+++ Backend/ApiImagenes/ApiImagenesApp/models.py
@@ -1,3 +1,21 @@
+from distutils.command.upload import upload
+from django.contrib.auth.models import User
+import email
+from pyexpat import model
+from statistics import mode
 from django.db import models
 
 # Create your models here.
+class Persona(models.Model):
+    nombre = models.CharField(max_length=60, blank=False, null=False)
+    identificacion = models.BigIntegerField(blank=False, null=False)
+    email = models.EmailField(max_length=254, blank=False, null=False)
+    telefono = models.BigIntegerField(blank=False, null=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, blank=False, null=False)
+
+class Marco(models.Model):
+    nombre = models.CharField(max_length=60, blank=False, null=False)
+    fecha = models.DateField(auto_now=True, blank=False, null=False)
+    evento = models.CharField(max_length=60, blank=False, null=False)
+    personaID = models.ForeignKey('Persona', on_delete=models.SET_NULL, blank=False, null=True)
+    imagen = models.ImageField(upload_to='./marcos', blank=False, null=False)
